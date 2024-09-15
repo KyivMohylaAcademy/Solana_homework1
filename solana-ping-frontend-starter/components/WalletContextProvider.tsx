9054d8bbd43b470e983e39fd955cffa50c0a4802
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-09-15 19:44:52 +0300

    Submit all changes

--- solana-ping-frontend-starter/components/WalletContextProvider.tsx
+++ solana-ping-frontend-starter/components/WalletContextProvider.tsx
@@ -5,12 +5,12 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { SolflareWalletAdapter, PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 require("@solana/wallet-adapter-react-ui/styles.css");
  
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
 const endpoint = clusterApiUrl("devnet");
-const wallets = [new PhantomWalletAdapter()]
+const wallets = [new SolflareWalletAdapter(), new PhantomWalletAdapter()]
 
   return (
     <ConnectionProvider endpoint={endpoint}>
