2ca5633e2fad924186a622ca1517a0b534b11dae
Maksym Karpinskyi <karpinskyi@DESKTOP-4A7OTK3>
2024-09-19 06:19:00 +0300

    Complete lab1

---
+++ generate-keypair.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+const { getKeypairFromEnvironment } = require('@solana-developers/helpers');
+ 
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+ 
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`,
+);
